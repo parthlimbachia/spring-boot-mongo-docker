f65ce5907d7d22142696d3c062b812425a93a58d
Parth Limbachia <parthlimbachia@gmail.com>
2023-08-28 00:49:54 +0530

    Updated replacetag.sh

--- replacetag.sh
+++ replacetag.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 NEW_TAG=$1
 DOCKER_REGISTRY=parthlimbachia1985
+REPO=spring-boot-mongo
 IMAGE_TAG=$(cat springBootMongo.yml | grep $DOCKER_REGISTRY | awk -F ':' '{print $3}')
-sed -i "s/\$IMAGE_TAG/\$NEW_TAG/g" springBootMongo.yml
+sed -i "s@$DOCKER_REGISTRY/$REPO:$IMAGE_TAG@$DOCKER_REGISTRY/$REPO:$NEW_TAG@g" springBootMongo.yml
